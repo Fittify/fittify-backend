e78d8edcfa264be56591d9b6241de1751f77becd
bartkalit <bartkalit@gmail.com>
2023-05-28 20:42:47 +0200

    update CI

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'fittify.apps',
+    'fittify.apps.FittifyConfig',
 ]
 
 REST_FRAMEWORK = {
