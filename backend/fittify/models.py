3940f5906ec4f5b255be8cf43319b267285d21a1
LeonKranner <leon.kranner@freenet.de>
2023-06-12 21:51:26 +0200

    Added_lobby_model

--- backend/fittify/models.py
+++ backend/fittify/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+from datetime import date
+
 # Create your models here.
+class Lobby(models.Model):
+    title = models.CharField(max_length=50)
+    sport = models.CharField(max_length=20)
+    players = models.ManyToManyField(User)
+    date_of_play = models.DateField(default = date.today)
+    due_date = models.DateField()
+    description =models.CharField(max_length = 500)
+    max_number_of_players = models.IntegerField()
