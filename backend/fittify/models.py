b6c001eb5c5a7dcba9b321a186002d0c33334d59
LeonKranner <leon.kranner@freenet.de>
2023-06-15 11:51:42 +0200

    Lobby-Modelv1

--- backend/fittify/models.py
+++ backend/fittify/models.py
@@ -5,10 +5,11 @@ from datetime import date
 
 # Create your models here.
 class Lobby(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=50, default = "Sport")
     sport = models.CharField(max_length=20)
     players = models.ManyToManyField(User)
     date_of_play = models.DateField(default = date.today)
     due_date = models.DateField()
     description =models.CharField(max_length = 500)
     max_number_of_players = models.IntegerField()
+    place = models.CharField(max_length = 30)
